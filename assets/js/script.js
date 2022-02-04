4847aec1e8a8d006e50fcd540bd6d0d784716865
Tanya Rushing <tanyarushing@Tanyas-Air.lan>
2022-02-03 21:34:00 -0500

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,50 @@
 // TODO: Declare any global variables we need
-
+let totalCount = 0
+let headsCount = 0
+let tailsCount = 0
 
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
     console.log('Hi')
+//DOM elements
+let flipBtn = document.getElementById('flip')
+let clearBtn = document.getElementById('clear')
+
+//Scoreboard cells
+let headsCountData = document.getElementById('heads')
+let headsPercentData = document.getElementById('heads-percent')
+let tailsCountData = document.getElementById('tails')
+let tailsPercentData = document.getElementById('tails-percent')
+//IMG
+let pennyImg = document.getElementById('penny-img')
+//message
+let message = document.getElementById('message')
 
     // TODO: Add event listener and handler for flip and clear buttons
 
     // Flip Button Click Handler
+    flipBtn.addEventListener('click', function(){
         // TODO: Determine flip outcome
+        totalCount++
+        let flippedHeads = Math.random() < 0.5
+    
+        // if check - what did they flip
+        if(flippedHeads) {
+            pennyImg.src = 'assets/images/penny-head.jpg'
+            message.textContent = "You flipped heads!"
+            headsCount++
+            headsCountData.textContent = headsCount
+        } else{
+            pennyImg.src = 'assets/images/penny-tails.jpg'
+            message.textContent = "You flipped tails!"
+            tailsCountData.textContent = tailsCount
+        }
+
+        let headsPercentage = Math.round((headsCount/totalCount) * 100)
+        let tailsPercentage = Math.round((tailsCount/totalCount) *100)
+        headsPercentData.textContent = `${headsPercentData}`
+        tailsPercentData.textContent = `${tailsPercentData}`
         // TODO: Update image and status message in the DOM
 
         // Update the scorboard
@@ -23,5 +58,5 @@ document.addEventListener('DOMContentLoaded', function () {
     // Clear Button Click Handler
         // TODO: Reset global variables to 0
         // TODO: Update the scoreboard (same logic as in flip button click handler)
-
+    });
 })
